eba753994b943855243f5360b73f2b3fb35f2e2b
Jon Wayne Parrott <jjramone13@gmail.com>
2014-09-23 20:06:36 -0400

    Adding setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+VERSION = '0.0.1'
+
+setup(
+    name="Darth Vendor",
+    version=VERSION,
+    author='Jon Wayne Parrott, [proppy], [webmaven]',
+    author_email='jjramone13@gmail.com, [proppy], [webmaven]',
+    maintainer='Jon Wayne Parrott',
+    maintainer_email='jon.wayne.parrott@gmail.com',
+    description='Third-party vendoring helper for Google App Engine and other sandboxed python environments.',
+    url='https://github.com/jonparrott/Darth-Vendor',
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+    ],
+    packages=['darth_vendor'],
+    install_requires=[
+    ],
+)
